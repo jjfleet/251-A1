0e44eb8801b38e4e0a11655d8eae0715dc4703d0
Glen Ward <arcangw@gmail.com>
2018-09-06 15:10:03 +1200

    Added partial persistence. Comment out 'saveTaskList/loadTaskList' to run code

--- src/main/java/nz/ac/massey/cs/TaskList.java
+++ src/main/java/nz/ac/massey/cs/TaskList.java
@@ -1,5 +1,11 @@
 package nz.ac.massey.cs;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
@@ -37,5 +43,42 @@ public class TaskList implements Serializable {
     	return collection.stream().filter(task -> !task.isComplete()).count();
     }
     
+    private static void saveTaskList(TaskList YourObject, String filePath) throws IOException {
+    	ObjectOutputStream outputStream = null;
+    	try {
+    		outputStream = new ObjectOutputStream(new FileOutputStream(filePath));
+    		outputStream.writeObject(YourObject);
+    	}
+    	catch(FileNotFoundException ex) {
+    		ex.printStackTrace();
+    	}
+    	catch(IOException ex) {
+    		ex.printStackTrace();
+    	}
+    	finally {
+    		try {
+    			if(outputStream != null) {
+    				outputStream.flush();
+    				outputStream.close();
+    			}
+    		}
+    		catch(IOException ex) {
+    			ex.printStackTrace();
+    		}
+    	}
+    }
     
+    public static TaskList loadTaskList(String filePath) throws IOException {
+    	try {
+    		FileInputStream fileIn = new FileInputStream(filePath);
+    		ObjectInputStream in = new ObjectInputStream(fileIn);
+    		return (TaskList) in.readObject();
+    	}
+    	catch(FileNotFoundException ex) {
+    		ex.printStackTrace();
+    	}
+    	catch(IOException ex) {
+    		ex.printStackTrace();
+    	}
+    }
 }
